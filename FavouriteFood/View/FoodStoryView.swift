ce95973aeb3287dc238a2802cf804bcec2352c83
Henry Lee <nynxzx@gmail.com>
2021-03-17 23:55:35 +1000

    Refactor naming

--- FavouriteFood/View/FoodStoryView.swift
+++ FavouriteFood/View/FoodStoryView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct FoodStoryView: View {
-    let description:String
+    let story:String
     
     var body: some View {
         ZStack{
             Rectangle()
                 .foregroundColor(.white)
             VStack{
-                Text(description)
+                Text(story)
                     .padding()
             }
         }
@@ -28,6 +28,6 @@ struct FoodStoryView: View {
 
 struct FoodStoryView_Previews: PreviewProvider {
     static var previews: some View {
-        FoodStoryView(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+        FoodStoryView(story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
     }
 }
